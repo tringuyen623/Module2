fbe53202801d7ae945f52df9c420769c47066d8e
tringuyen623 <tringuyen2391@gmail.com>
2019-12-08 11:13:47 +0700

    add submits Thuc hanh gio he thong

---
+++ Tong-quan-ung-dung-web/ThucHanh/server_time/index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Thời gian hiện tại</title>
+</head>
+<body>
+<h1> Bây giờ là: <?php echo date('Y-M-d h:i:s'); ?> </h1> 	
+</body>
+</html>
