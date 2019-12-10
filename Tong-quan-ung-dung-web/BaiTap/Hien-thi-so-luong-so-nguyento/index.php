e74b7db4a054dabddbb6649e053c637ac9c0a467
tringuyen623 <tringuyen2391@gmail.com>
2019-12-10 11:43:33 +0700

    update

--- Tong-quan-ung-dung-web/BaiTap/Hien-thi-so-luong-so-nguyento/index.php
+++ Tong-quan-ung-dung-web/BaiTap/Hien-thi-so-luong-so-nguyento/index.php
@@ -10,29 +10,33 @@
 
 <body>
     <form method="post">
-        <label for="choosenumber">Choose number of Prime number</label>
+        <label>Enter number of Prime number</label>
         <input type="text" name="size">
         <input type="submit">
 
     </form>
     <?php
 
-    $size = $_POST['size'];
-    $size = (int) $size;
+    $size = $_POST['size'] ?? 0;
     $N = 2;
-    for ($i = 0; $i < $size;) {
-        $count = 0;
-        for ($j = 2; $j * $j <= $N; $j++) {
-            if ($N % $j ==   0) {
-                $count++;
+    if (empty($_POST['size'])|| !is_numeric($size)) {
+        echo "Please enter the size of Prime number";
+    } else {
+        for ($i = 0; $i < $size;) {
+            $count = 0;
+            for ($j = 2; $j * $j <= $N; $j++) {
+                if ($N % $j ==   0) {
+                    $count++;
+                }
             }
+            if ($count === 0) {
+                echo "$N<br>";
+                $i++;
+            }
+            $N++;
         }
-        if ($count === 0) {
-            echo "$N<br>";
-            $i++;
-        }
-        $N++;
     }
+
     ?>
 </body>
 
