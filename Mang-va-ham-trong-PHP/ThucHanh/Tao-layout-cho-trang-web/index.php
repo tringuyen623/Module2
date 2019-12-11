6864cddce5945a296ee83af642bbd09acf35e8f7
tringuyen623 <tringuyen2391@gmail.com>
2019-12-11 10:17:30 +0700

    submit Tao layout cho web

---
+++ Mang-va-ham-trong-PHP/ThucHanh/Tao-layout-cho-trang-web/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Tạo layout cho trang web </title>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <header><?php include "header.php"; ?></header>
+        <nav><?php include "nav.php"; ?></nav>
+        <article><?php include "content.php"; ?></article>
+        <footer><?php include "footer.php"; ?></footer>
+    </div>
+</body>
+</html>
